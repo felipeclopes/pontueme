dd8c4c12f715c4cea84888ecebc262e3e8c86c16
felipeclopes <felipelopes10@gmail.com>
2012-12-07 02:46:05 -0200

    sixing migrations

--- db/migrate/20121207004122_create_views_for_user_business_points.rb
+++ db/migrate/20121207004122_create_views_for_user_business_points.rb
@@ -6,8 +6,8 @@ class CreateViewsForUserBusinessPoints < ActiveRecord::Migration
         u.id AS user_id, 
         b.id AS business_id,
         SUM(c.points) AS total_points, 
-        COALESCE((SELECT SUM(co.points) FROM coupons co JOIN benefits be ON be.id == co.benefit_id WHERE co.user_id = u.id AND be.business_id = b.id), 0) AS benefit_points,
-        SUM(c.points) - COALESCE((SELECT SUM(co.points) FROM  coupons co JOIN benefits be ON be.id == co.benefit_id WHERE co.user_id = u.id AND be.business_id = b.id),0) AS points
+        COALESCE((SELECT SUM(co.points) FROM coupons co JOIN benefits be ON be.id = co.benefit_id WHERE co.user_id = u.id AND be.business_id = b.id), 0) AS benefit_points,
+        SUM(c.points) - COALESCE((SELECT SUM(co.points) FROM  coupons co JOIN benefits be ON be.id = co.benefit_id WHERE co.user_id = u.id AND be.business_id = b.id),0) AS points
       FROM users u
         JOIN checkins c ON c.user_id = u.id
         JOIN businesses b ON b.id = c.business_id
--- db/migrate/20121207004820_create_view_for_card_business_points.rb
+++ db/migrate/20121207004820_create_view_for_card_business_points.rb
@@ -6,8 +6,8 @@ class CreateViewForCardBusinessPoints < ActiveRecord::Migration
         ca.id AS card_id, 
         b.id AS business_id,
         SUM(c.points) AS total_points, 
-        COALESCE((SELECT SUM(co.points) FROM coupons co JOIN benefits be ON be.id == co.benefit_id WHERE co.card_id = c.id AND be.business_id = b.id), 0) AS benefit_points,
-        SUM(c.points) - COALESCE((SELECT SUM(co.points) FROM coupons co JOIN benefits be ON be.id == co.benefit_id WHERE co.card_id = c.id AND be.business_id = b.id),0) AS points
+        COALESCE((SELECT SUM(co.points) FROM coupons co JOIN benefits be ON be.id = co.benefit_id WHERE co.card_id = c.id AND be.business_id = b.id), 0) AS benefit_points,
+        SUM(c.points) - COALESCE((SELECT SUM(co.points) FROM coupons co JOIN benefits be ON be.id = co.benefit_id WHERE co.card_id = c.id AND be.business_id = b.id),0) AS points
       FROM cards ca
         JOIN checkins c ON c.card_id = ca.id
         JOIN businesses b ON b.id = c.business_id
